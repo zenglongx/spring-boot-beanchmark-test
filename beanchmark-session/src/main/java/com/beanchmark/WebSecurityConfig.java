d56223aa24111d823823decae9f1d1bd9e303eff
邢增龙 <dj008727xingzenglong@djbx.com>
2020-04-20 15:53:52 +0800

    commit

---
+++ beanchmark-session/src/main/java/com/beanchmark/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.beanchmark;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder(4);
+//        return new MessageDigestPasswordEncoder("MD5");
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+    }
+
+}
